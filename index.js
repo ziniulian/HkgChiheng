847936f98f3555a843581855dba8acae8a2a5989
ziniulian <ziniulian@163.com>
2018-09-06 15:29:51 +0800

    加入数据库管理模块

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("lzr");
 
 // LZR 子模块加载
 LZR.load([
+	"LZR.HTML",
 	"LZR.Node.Srv"
 ]);
 
@@ -12,17 +13,39 @@ var srv = new LZR.Node.Srv ({
 	port: process.env.OPENSHIFT_NODEJS_PORT || 80
 });
 
+// 需要用到的工具
+var tools = {
+	mUrl: LZR.HTML.domain,
+	dm: null	// 数据库管理模块
+};
+
 // LOGO图片
 srv.ro.get("/favicon.ico", function (req, res) {
-	res.sendFile("Logo.png", {
-		root: "./"
-	});
+	res.redirect(tools.mUrl + "favicon.ico");
+});
+
+// 记录访问信息到 dm 模块中
+srv.ro.get("/myNam/", function (req, res, next) {
+	var t = tools.dm.getTls();
+	t.qryRo.db.add( req, res, next, null, {
+		ip: t.utNode.getClientIp(req),
+		tim: t.utTim.getTim()
+	}, true );
 });
 
 // 返回服务名
 srv.ro.get("/myNam/", function (req, res) {
 	res.send("HKG");
 });
+
+// 公共样式
+srv.ro.get("/base.css", function (req, res) {
+	res.redirect(tools.mUrl + "css/common/base.css");
+});
+
+// 数据库管理模块
+tools.dm = require("./DbMgr");
+srv.use("/DbMgr/", tools.dm);
 
 // 静态主页设置
 srv.ro.setStaticDir("/", "./web");
